597ffdd93102a7515aad438587fd645fff32d2a1
Andrew Wojciechowski <wojciechowskia@msoe.edu>
2019-01-01 11:11:33 -0600

    General refactoring of PointGenerator

--- _plugins/PointGenerator.rb
+++ _plugins/PointGenerator.rb
@@ -3,20 +3,20 @@ require 'find'
 require 'rubyXL'
 require 'json'
 
-class PointGenerator < Jekyll::Generator 
-  def generate(site)
-    make_data_directory_if_necessary()
-    excel_files = get_all_excel_file_names()
+class PointGenerator < Jekyll::Generator
+  def generate
+    make_data_directory
+    excel_files = find_all_excel_file_names
     json = parse_excel_files(excel_files)
     create_json_file(json)
   end
 
-  def get_all_excel_file_names()
+  def find_all_excel_file_names
     result = []
     Find.find(parent_directory) do |path|
       result << path if path =~ /.*\.xlsx$/
     end
-    return result
+    result
   end
 
   def parse_excel_files(excel_files_names)
@@ -25,63 +25,58 @@ class PointGenerator < Jekyll::Generator
     worksheet = workbook[0]
     meetings = parse_meetings(worksheet)
     students = parse_students(worksheet)
-    return JSON.pretty_generate({meetings: meetings, students: students})
+    JSON.pretty_generate(meetings: meetings, students: students)
   end
 
   def parse_meetings(worksheet)
     result = []
     worksheet[0].cells.each do |cell|
-      if cell && cell.value && "Totals".casecmp(cell.value) != 0
-        result << cell.value
-      end
+      result << cell.value if cell && cell.value && 'Totals'.casecmp(cell.value) != 0
     end
-    return result
+    result
   end
 
   def parse_students(worksheet)
     result = []
-    worksheet.each { |row|
-      name = ""
+    worksheet.each do |row|
+      name = ''
       point_breakdown = []
       total_points = 0
-      row.cells.each { |cell| 
-        if cell && cell.row != 0 && cell.column === 0
+      row.cells.each do |cell|
+        if cell && cell.row != 0 && cell.column.zero?
           name = cell.value
-        elsif cell && cell.row != 0 && cell.column === row.cells.last.column
+        elsif cell && cell.row != 0 && cell.column == row.cells.last.column
           total_points = cell.value
         else
-          if cell && cell.value
-            point_breakdown << cell.value
-          else
-            point_breakdown << 0
-          end
+          point_breakdown << if cell && cell.value
+                               cell.value
+                             else
+                               0
+                             end
         end
-      }
-      if name != ""
-        result << StudentData.new(name, point_breakdown, total_points)
       end
-    }
-    result = result.map{|data| {name: data.name, pointBreakdown: data.point_breakdown, pointTotal: data.total_points}}
-    result = result.sort_by {|hsh| hsh[:pointTotal]}.reverse!
-    return result
+      result << StudentData.new(name, point_breakdown, total_points) if name != ''
+    end
+    result = result.map { |data| { name: data.name, pointBreakdown: data.point_breakdown, pointTotal: data.total_points } }
+    result.sort_by { |hsh| hsh[:pointTotal] }.reverse!
   end
 
   def create_json_file(json)
     file_name = "#{path}/points.json"
-    open(file_name, "wb") do |file|
+    File.open(file_name, 'wb') do |file|
       file << json
     end
   end
-    
+
   def path
-    return File.join(parent_directory, "_data")
+    File.join(parent_directory, '_data')
   end
 
   def parent_directory
-    return File.expand_path(".", Dir.pwd)
+    File.expand_path('.', Dir.pwd)
   end
 
-  def make_data_directory_if_necessary()
+  def make_data_directory
     Dir.mkdir(path) unless Dir.exist?(path)
   end
 end
@@ -93,15 +88,9 @@ class StudentData
     @total_points = total_points
   end
 
-  def name
-    @name
-  end
+  attr_reader :name
 
-  def point_breakdown
-    @point_breakdown
-  end
+  attr_reader :point_breakdown
 
-  def total_points
-    @total_points
-  end
+  attr_reader :total_points
 end
