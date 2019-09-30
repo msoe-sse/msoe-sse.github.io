ea110dde66a92690c8cddddaba9ece446abf9c8d
Hunter Turcin <huntertur@gmail.com>
2019-09-30 18:32:03 -0500

    Implement ability to spin the raffle machine

--- assets/js/raffle.js
+++ assets/js/raffle.js
@@ -19,11 +19,42 @@ document.addEventListener("DOMContentLoaded", () => {
         });
 });
 
+function getRandomArrayItem(array) {
+    /* There does not appear to be a standard way to pick a random item from
+       an Array. Note: Since Array.length is one more than the maximum index of
+       the Array, Math.random() not ever returning 1 won't be a problem when
+       flooring the result to an integer, as it takes care of not returning
+       one over the highest index. */
+    return array[Math.floor(Math.random() * array.length)];
+}
+
 function generateMachine(data) {
+    /* The DOM looks like this:
+
+         div#raffle (= container)
+           button#spin (= spinButton)
+           p#winner (= winnerElement)
+    */
     let container = document.querySelector('#raffle');
+    let entries = [];
 
     data.students.forEach(student => {
-        let entry = container.appendChild(document.createElement('p'));
-        entry.textContent = `${student.name} has ${student.pointTotal}`;
+        /* The more total points a student has, the more entries they have in
+           the raffle. */
+        for(let i = 0; i < student.pointTotal; i += 1) {
+            entries.push(student.name);
+        }
+    });
+
+    let spinButton = container.appendChild(document.createElement('button'));
+    spinButton.type = 'button';
+    spinButton.id = 'spin';
+    spinButton.textContent = 'Spin';
+
+    let winnerElement = container.appendChild(document.createElement('p'));
+    winnerElement.id = 'winner';
+
+    spinButton.addEventListener('click', () => {
+        winnerElement.textContent = getRandomArrayItem(entries);
     });
 }
