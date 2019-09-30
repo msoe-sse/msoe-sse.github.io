29d00f3cd17faa7785a4ff3dc9c17de8ad0d1c50
Hunter Turcin <huntertur@gmail.com>
2019-09-30 17:58:34 -0500

    Fetch students' names and point totals

--- assets/js/raffle.js
+++ assets/js/raffle.js
@@ -7,6 +7,23 @@ sitemap:
 
 document.addEventListener("DOMContentLoaded", () => {
     let apiUrl = '{{ site.APIBaseUrl }}';
+    let container = document.querySelector('#raffle');
 
-    document.querySelector('#raffle').textContent = 'Test! ' + apiUrl;
+    fetch(`${apiUrl}/points`)
+        .then(response => response.json())
+        .then(data => generateMachine(data))
+        .catch(() => {
+            container.textContent
+                = ('There was an error while contacting the points service. ' +
+                   'Please try again later.')
+        });
 });
+
+function generateMachine(data) {
+    let container = document.querySelector('#raffle');
+
+    data.students.forEach(student => {
+        let entry = container.appendChild(document.createElement('p'));
+        entry.textContent = `${student.name} has ${student.pointTotal}`;
+    });
+}
